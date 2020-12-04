a8becd15fd768c41b6df961c3284e9d670501769
Mezrah <ishai>
2020-12-04 17:08:14 -0500

    corrected 100 max number issue

--- countdownpercent.py
+++ countdownpercent.py
@@ -1,25 +1,24 @@
+
 import os 
 import random 
 import time
 
 
 def clear():
-	os.system('clear')
+    os.system('clear')
 
 percent = 0
 clear()	
 print(percent, '%')
 time.sleep(1)
 while percent < 100:
-	clear()
-	decimal = random.randrange(9) / 10
-	integer = random.randrange(10)
-	increment = decimal + integer
-	leftover = 100 - percent
-	if increment > leftover:
-		increment = random.randrange(int(leftover * 10))
-		percent += increment / 10
-	percent += increment
-	print(round(percent, 1), '%')
-	time.sleep(1)
-	clear()
+    clear()
+    increment = random.randrange(109) / 10
+    leftover = 100 - percent
+    if increment > leftover:
+        increment = random.randrange(int(leftover * 10)) / 10
+        percent += increment
+    percent += increment
+    print(round(min(percent, 100), 1), '%')
+    time.sleep(1)
+    clear()
