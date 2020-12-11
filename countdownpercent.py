b58fcf1b726e09b323988325896b74a61c88976e
Isha <imasada@vtcap.org>
2020-12-11 08:51:56 -0500

    added float feature and cleaned up code

--- countdownpercent.py
+++ countdownpercent.py
@@ -12,13 +12,10 @@ print(percent, '%')
 time.sleep(1)
 while percent < 100:
 	clear()
-	#decimal = random.randrange(9)/10
+	decimal = random.randrange(9)/10
 	integer = random.randrange(10)
-	#increment = decimal + integer
-	leftover = 100 - percent
-	if integer > leftover:
-		integer = random.randrange(leftover)
-	percent += integer 
-	print(percent, '%')
+	increment = decimal + integer
+	percent += increment 
+	print(round(min(percent, 100), 2),  '%')
 	time.sleep(1)
 	clear()
